576a0ab4124414e23fc68fc563b4b5ba7f209fd6
Logan Green <loganagreen95@gmail.com>
2020-10-25 18:27:42 -0400

    Add ups item contents

--- lib/api/ups.rb
+++ lib/api/ups.rb
@@ -2,6 +2,25 @@ module RubyShipper
     module API
         class UPS
             def ship(shipping_address: nil, billing_address: nil, return_address: nil, package: nil, payment: nil, options: nil)
+                packages = "" 
+                package[:items_array].each do |i|
+                  descr = i[:contents]
+                  packages += "<Product><Description>#{descr}</Description>"\
+                  "<Unit>"\
+                    "<Number>#{i[:quantity]}</Number>"\
+                    "<Value>#{i[:value]}</Value>"\
+                        "<UnitOfMeasurement>"\
+                            "<Code>OTH</Code>"\
+                            "<Description>UOM</Description>"\
+                        "</UnitOfMeasurement>"\
+                    "</Unit>"\
+                    "<CommodityCode>#{i[:tariff_code]}</CommodityCode>"\
+                    "<OriginCountryCode>#{i[:manufacture_country]}</OriginCountryCode>"\
+                    "</Product>"
+                end
+            end
+
+            def rate(destination: nil, origin: nil)
             end
         end
     end
