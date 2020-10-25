60ab52cde7f15f0af1321c1763314bbbce6b69cc
Logan Green <loganagreen95@gmail.com>
2020-10-25 19:07:08 -0400

    More validations for builders

--- lib/builder/address.rb
+++ lib/builder/address.rb
@@ -1,11 +1,16 @@
 module RubyShipper
     module Builder
         class Address
-            attr_reader :name, :attention, :address1, :address2, :city, :state, :zip, :country
-            attr_writer :name, :attention, :address1, :address2, :city, :state, :zip, :country
+            attr_reader :name, :attention, :address1, :address2, address3, :city, :state, :zip, :country
+            attr_writer :name, :attention, :address1, :address2, address3, :city, :state, :country
             
             def zip=
                 raise "Zip code for US must be over 4 digits" if self.country == "US" && self.zip.length < 5
+            end
+
+            def initialize
+                raise "Name or attention must be present" if self.name.nil? && self.attention.nil?
+                raise "Must have a street address" if self.address1.nil? && self.address2.nil? && self.adress3.nil?
             end
         end
     end
